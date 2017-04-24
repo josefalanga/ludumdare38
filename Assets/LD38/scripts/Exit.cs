059ad6e75ffff4bc44ca55916846e5678b0c9d75
Jose Falanga <jose.falanga@gmail.com>
2017-04-24 09:14:08 -0300

    monday bugfix

--- Assets/LD38/scripts/Exit.cs
+++ Assets/LD38/scripts/Exit.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour {
 	void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.Escape)) {
         	Application.Quit();
-   		 }
+		}
+		if (Input.GetKeyDown(KeyCode.R)) {
+        	SceneManager.LoadScene(0);
+		}
 	}
 }
